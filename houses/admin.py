40e4738b70ea3af5b7ee9705caf8e2e01800da30
enterspacebs <anotherhell48@gmail.com>
2023-07-10 02:04:14 +0900

    wake up early

--- houses/admin.py
+++ houses/admin.py
@@ -1,18 +1,12 @@
 from django.contrib import admin
 from .models import House
 
+
 # Register your models here.
-@admin.register(House) # admin name; Houses 장고가 붙여줌 '-s'
+@admin.register(House)  # admin name; Houses 장고가 붙여줌 '-s'
 class HouseAdmin(admin.ModelAdmin):
-    fields = ("name", "address", ("price_per_night", "pets_allowed"),)
-    list_display = [
-        "name",
-        "price_per_night",
-        "address",
-        "pets_allowed"
-    ]
     list_filter = ["price_per_night", "pets_allowed"]
-    search_fields = ["address__startswith"] # 정확히 그 검색어 아니게 정함. contains, endswith, 대문자 소문자 등등
+    search_fields = [
+        "address__startswith"
+    ]  # 정확히 그 검색어 아니게 정함. contains, endswith, 대문자 소문자 등등
     # exclude = ("price_per_night",)
-    list_display_links = ("name", "address",)
-    list_editable = ("pets_allowed", )
--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=150, editable=False)
+    last_name = models.CharField(
+        max_length=150, editable=False
+    )  # editable 속성을 달면 관리자 패널에 안 나옴
+
+    name = models.CharField(
+        max_length=150,
+        default="",  # 이미 만들어져있는 유저 때문에 name 필드를 추가하면 스트링을 넣어줘야 한다.
+    )  # models.py 수정할 때마다 makemigrations, migrate 해줘야 함; db와 같지 않으니까
+
+    is_host = models.BooleanField(
+        default=False
+    )  # default=False or null=True, 이미 만들어져있는 유저 때문에 is_host 필드를 추가하면 NULL이 되기 때문에
