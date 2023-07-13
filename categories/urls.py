d0ffc6ce92fb1163b6a7acb2ab2158e79406f694
enterspacebs <anotherhell48@gmail.com>
2023-07-14 01:13:55 +0900

    ViewSet

--- categories/urls.py
+++ categories/urls.py
@@ -2,6 +2,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.Categories.as_view()),
-    path("<int:pk>", views.CategoryDetail.as_view()),
+    path(
+        "",
+        views.CategoryViewSet.as_view(
+            {
+                "get": "list",
+                "post": "create",
+            }
+        ),
+    ),  # ViewSet의 메소드와 사용자가 나한테 보낼 HTTP 메소드를 연결해주면 땡.
+    # Categories
+    path(
+        "<int:pk>",  # URL에서 pk를 받을 수 있도록 해줘야 한다. ViewSet은 pk가 필수임.
+        views.CategoryViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+    ),  # CategoryDetail
 ]
