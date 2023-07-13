1aac6ccb9c8c7f6f9dadba0b8c5c73e87eb237d1
enterspacebs <anotherhell48@gmail.com>
2023-07-14 00:57:26 +0900

    MM

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
+from .models import Category
 
 
 class CategorySerializer(serializers.Serializer):
-    name = serializers.CharField(required=True)
-    kind = serializers.CharField()
+    class Meta:
+        model = Category
+        fields = "__all__"
