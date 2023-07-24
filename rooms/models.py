4d7c40e758bb342674d94acf4e3498bd93df1cb6
enterspacebs <anotherhell48@gmail.com>
2023-07-24 17:07:48 +0900

    categories, rooms, experiences

--- rooms/models.py
+++ rooms/models.py
@@ -67,6 +67,12 @@ class Amenity(CommonModel):
         null=True,
         blank=True,
     )
+    category = models.ForeignKey(
+        "categories.Category",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
 
     def __str__(self) -> str:
         return self.name
