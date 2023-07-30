7381645d2792eb0f8d21e1c159c7dad820e24b6d
enterspacebs <anotherhell48@gmail.com>
2023-07-30 20:46:33 +0900

    8.4 go

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,10 +2,40 @@ from django.contrib import admin
 from .models import Review
 
 
+class WordFilter(admin.SimpleListFilter):
+    title = "Filter by word!"
+
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),  # url에 모두 표시된다. 장고는 url을 읽고, 나에게 필터링한 결과를 준다.
+            ("great", "Great"),
+            ("awesome", "Awesome"),
+        ]
+
+    def queryset(self, request, reviews):
+        word = self.value()
+        # print(self.value()) 같이 하면 실제로 url을 읽어서 그걸 뽑아내지 않아도 된다. # print(request.get)의 출력값인 {'word': ['good']}을 가져와서 배열에서 단어를 빼내온다.
+        # print(dif(request)) # 단어로 필터링을 해보자.
+        # print(reviews) # 이 review들을 필터링한 다음 리턴해야 한다.
+        if word:
+            return reviews.filter(
+                payload__contatins=word
+            )  # models.py의 payload에 word가 포함되어 있는지 확인한다.
+        else:
+            reviews
+
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = (
         "__str__",
         "payload",
     )
-    list_filter = ("rating",)
+    list_filter = (
+        "rating",
+        "user__is_host",  # relationship으로도 필터링을 할 수 있다. foreign key를 통해 필터링을 할 수 있다.
+        "room__category",  # category가 또 다른 foreign key를 가지면 외래키의 외래키 filter도 가능하다. # room의 category 또한 foreign key이지만 필터링을 할 수 있다.
+        "room__pet_friendly",
+    )
--- rooms/admin.py
+++ rooms/admin.py
@@ -2,8 +2,29 @@ from django.contrib import admin
 from .models import Room, Amenity
 
 
+@admin.action(
+    description="Set all prices to zero"
+)  # 관리자 페이지를 구축할 때, 동시에 여러 값들을 리셋하거나 수정할 때 액션을 사용할 수 있다. excel에 export 하는 것도 가능하다.
+def reset_prices(
+    model_admin,
+    request,
+    rooms,
+):  # 세번째로 Queryset은 내가 선택한 모든 객체의 리스트다. 선택된 모든 객체를 주기에 그 객체들로 작업을 할 수 있다.
+    # 두번째는 request 객체로, 이 액션을 누가 호출했는지에 대한 정보를 갖고 있다. 슈퍼유저인지, 일반유저인지 등등. 따라서 슈퍼 유저가 아니면 이 액션을 호출할 수 없다.
+    # 첫번째는 액션을 호출하는 클래스다. # admin 액션은 3개의 매개변수로 호출된다.
+    for room in rooms.all():
+        room.price = 0
+        room.save()  # 이제 내가 선택하는 방들은 새로운 price로 0을 가지게 된다.
+        """ 그냥 함수를 만들고 admin.action decorator에 설명을 넣어 주고 admin 클래스 안의 actions에 그 함수를 추가하기만 하면 된다."""
+    # print(model_admin)
+    # print(dir(request))
+    # print(rooms)
+    # pass
+
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+    actions = (reset_prices,)
     list_display = (
         "name",
         "price",
@@ -25,7 +46,7 @@ class RoomAdmin(admin.ModelAdmin):
     )
 
     search_fields = (
-        "owner__username",  # owner의 username으로 검색한다.
+        "^owner__username",  # ^는 username으로 시작하는 내용 찾음 # owner의 username으로 검색한다.
         # "^name",  # name으로 시작하는 것을 검색한다.
         # "=price",  # price로 시작하는 것을 검색한다. ^를 빼주면 price가 포함된 것을 검색한다.
         # # = (exact)는 정확히 일치하는 것을 검색한다.
