77495091e9643cbf538ae9faee3471454ab1e4bd
enterspacebs <anotherhell48@gmail.com>
2023-08-01 06:28:57 +0900

    studying early starts

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,5 @@
+# catergories를 별도의 app에 두는 이유는 categories가 두 개의 다른 app에서 사용될 수 있기 때문이다.
+# room과 experience가 categories를 공유할 수 있다.
 from django.db import models
 from common.models import CommonModel
 
